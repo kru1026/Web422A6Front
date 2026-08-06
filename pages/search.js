cd9ae095dcecbe318fb3d8f7df43e74b81fc58c6
Keith <kru1026@gmail.com>
2026-08-06 13:19:50 -0400

    Aug 06, 1:20pm

--- pages/search.js
+++ pages/search.js
@@ -74,7 +74,7 @@ return (
         <Form.Label>Geo Location</Form.Label>
         <Form.Control type="text" placeholder="" name="geoLocation" {...register ("geoLocation")}/>
         <Form.Text className="text-muted">
-        Case Sensitive String (ie &quot;Europe&quot;, &quot;France&quot;, &quot;Paris&quot;, &quot;China&quot;, &quot;New York&quot;, etc.), with multiple values separated by the | operator
+        Case Sensitive String (ie &quot;Europe&quot;, &quot;France&quot;, &quot;Paris&quot;, &quot;China&quot;, &quot;New York&quot;, etc.)
       </Form.Text>
       </Form.Group>
     </Col>
@@ -83,7 +83,7 @@ return (
         <Form.Label>Medium</Form.Label>
         <Form.Control type="text" placeholder="" name="medium" {...register ("medium")}/>
         <Form.Text className="text-muted">
-        Case Sensitive String (ie: &quot;Ceramics&quot;, &quot;Furniture&quot;, &quot;Paintings&quot;, &quot;Sculpture&quot;, &quot;Textiles&quot;, etc.), with multiple values separated by the | operator
+        Case Sensitive String (ie: &quot;Ceramics&quot;, &quot;Furniture&quot;, &quot;Paintings&quot;, &quot;Sculpture&quot;, &quot;Textiles&quot;, etc.)
       </Form.Text>
       </Form.Group>
     </Col>
